8936ca6eeeb685edd0c9ba20690f3ba3e671b750
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-02-20 16:19:32 -0300

    Invert Tree (C++)

---
+++ tree/InvertTree.cpp
@@ -0,0 +1,35 @@
+#include <queue>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        std::queue<TreeNode*> queue;
+        queue.push(root);
+        while (!queue.empty()) {
+            TreeNode* node = queue.front();
+            queue.pop();
+
+            if (node != nullptr) {
+                TreeNode* temp = node->left;
+                node->left = node->right;
+                node->right = temp;
+
+                queue.push(node->left);
+                queue.push(node->right);
+            }
+        }
+
+        return root;
+    }
+};
