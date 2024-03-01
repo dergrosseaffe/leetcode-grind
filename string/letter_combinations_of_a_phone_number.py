42fe9e437b1ae1ca76fe440db93903a5e58488b9
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-03-01 12:24:43 -0300

    Letter Combinations of a Phone Number (Python)

---
+++ string/letter_combinations_of_a_phone_number.py
@@ -0,0 +1,28 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+
+        dic = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        def backtrack(index, path):
+            if index == len(digits):
+                combinations.append(''.join(path))
+                return
+
+            for char in dic[digits[index]]:
+                path.append(char)
+                backtrack(index + 1, path)
+                path.pop()
+
+        combinations = []
+        backtrack(0, [])
+        return combinations
