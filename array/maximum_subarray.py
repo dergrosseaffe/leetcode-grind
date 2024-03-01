c61a70d814f48b870970be531347313244a5daa2
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-03-01 12:45:30 -0300

    Maximum Subarray (Python)

---
+++ array/maximum_subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_max = global_max = nums[0]
+        for i in range(1, len(nums)):
+            current_max = max(nums[i], current_max + nums[i])
+            global_max  = max(global_max, current_max)
+
+        return global_max
