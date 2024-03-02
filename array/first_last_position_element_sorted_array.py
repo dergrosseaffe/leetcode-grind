66495f855a906d9ed0c2c52926d677940b7b3066
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-03-02 19:59:12 -0300

    First and Last Position of Element in Sorted Array (Improved) (Python)

--- array/first_last_position_element_sorted_array.py
+++ array/first_last_position_element_sorted_array.py
@@ -1,26 +1,38 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
 
-        def rangeBinarySearch(left: int, right: int) -> List[int]:
-            if left == right: return [-1, -1]
+        def findStartIndex(left: int, right: int) -> int:
+            index = -1
 
-            middle = left + (right - left) // 2
+            while left <= right:
+                mid = left + (right - left) // 2
 
-            if nums[middle] == target:
-                left = right = middle
+                if nums[mid] >= target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
 
-                # expand search left
-                while left > 0 and nums[left-1] == target:
-                    left -= 1
+                if nums[mid] == target:
+                    index = mid
 
-                # expand search right
-                while right < len(nums) - 1 and nums[right+1] == target:
-                    right += 1
+            return index
 
-                return [left, right]
-            elif nums[middle] > target:
-                return rangeBinarySearch(left, middle)
-            else:
-                return rangeBinarySearch(middle + 1, right)
+        def findEndIndex(left: int, right: int) -> int:
+            index = -1
 
-        return rangeBinarySearch(0, len(nums))
+            while left <= right:
+                mid = left + (right - left) // 2
+
+                if nums[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+                if nums[mid] == target:
+                    index = mid
+
+            return index
+
+
+        l = len(nums) - 1
+        return [findStartIndex(0, l), findEndIndex(0, l)]
