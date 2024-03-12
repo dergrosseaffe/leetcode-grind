a70ee31ccdf8b0aa9d46c315894c6bdd66ec2857
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-03-12 09:25:35 -0300

    3Sum (Python)

---
+++ array/3sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        S = set()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            complements = set()
+            for j in range(i + 1, len(nums)):
+                complement = -nums[i] - nums[j]
+                if complement in complements:
+                    triplet = (nums[i], nums[j], complement)
+                    if triplet not in S:
+                        S.add(triplet)
+
+                complements.add(nums[j])
+
+        return S
