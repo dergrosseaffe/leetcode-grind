060c910208c45d77c5fe216fe1a1ccf6fc166b7c
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-03-12 11:38:26 -0300

    Accounts Merge (Python)

---
+++ array/accounts_merge.py
@@ -0,0 +1,44 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+
+    def find(self, query):
+        if query not in self.parent:
+            self.parent[query] = query
+        elif self.parent[query] != query:
+            self.parent[query] = self.find(self.parent[query])
+
+        return self.parent[query]
+
+    def union(self, a, b):
+        rootA = self.find(a)
+        rootB = self.find(b)
+        if rootA != rootB:
+            self.parent[rootA] = rootB
+
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind()
+        emailToName = {}
+
+        for account in accounts:
+            name = account[0]
+            firstEmail = account[1]
+            for email in account[1:]:
+                uf.union(email, firstEmail)     # merges all emails with first email
+                emailToName[email] = name
+
+        rootEmails = {}
+        for email in emailToName.keys():
+            rootEmail = uf.find(email)
+            if rootEmail not in rootEmails:
+                rootEmails[rootEmail] = []
+            rootEmails[rootEmail].append(email)
+
+        result = []
+        for rootEmail, emails in rootEmails.items():
+            name = emailToName[rootEmail]
+            result.append([name] + sorted(emails))
+
+        return result
