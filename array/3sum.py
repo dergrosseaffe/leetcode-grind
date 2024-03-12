09e08446c6025496813752070767f87259e5dfaf
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-03-12 09:26:29 -0300

    3Sum (Python)

--- array/3sum.py
+++ array/3sum.py
@@ -3,7 +3,7 @@ class Solution:
         nums.sort()
         S = set()
 
-        for i in range(len(nums) - 2):
+        for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
 
