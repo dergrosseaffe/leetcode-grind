93e9eb82bee4a85f7f416036f785f6e2d73d0533
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-03-18 17:13:18 -0300

    Rotate Array (Python)

---
+++ array/rotate_array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        k %= len(nums)
+
+        def rotate(left: int, right: int) -> None:
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        rotate(0, len(nums) - 1)
+        rotate(0, k - 1)
+        rotate(k, len(nums) - 1)
