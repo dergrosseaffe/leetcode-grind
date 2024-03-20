a0f5417b78f87039e7e93d16443bb6a75b6591b9
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-03-20 11:55:57 -0300

    Largest Number (Python)

---
+++ array/largest_number.py
@@ -0,0 +1,38 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        def merge(left: List[int], right: List[int]) -> List[int]:
+            result = []
+            li, ri = 0, 0
+
+            while li < len(left) and ri < len(right):
+                a = str(left[li]) + str(right[ri])
+                b = str(right[ri]) + str(left[li])
+
+                if int(a) > int(b):
+                    result.append(left[li])
+                    li += 1
+                else:
+                    result.append(right[ri])
+                    ri += 1
+
+            if li < len(left):
+                result.extend(left[li:])
+
+            if ri < len(right):
+                result.extend(right[ri:])
+
+            return result
+
+        def mergesort(nums: List[int]) -> List[int]:
+            if len(nums) == 1:
+                return nums
+
+            mid = len(nums) // 2
+            left = mergesort(nums[:mid])
+            right = mergesort(nums[mid:])
+
+            return merge(left, right)
+
+        result = ''.join([str(x) for x in mergesort(nums)])
+
+        return result if result[0] != "0" else "0"
