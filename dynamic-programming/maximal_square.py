42a6359a971f9e30fd14389ac0b3d117edaca8ea
Júlio César Martins dos Anjos Silva <julio.anjos@gmail.com>
2024-04-19 10:24:24 -0300

    Maximal Square (Python)

--- dynamic-programming/maximal_square.py
+++ dynamic-programming/maximal_square.py
@@ -2,7 +2,7 @@ class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         dp = [[0] * len(matrix[0]) for _ in range(len(matrix))]
 
-        max_size = 0
+        max_side = 0
         for row in range(len(matrix)):
             for col in range(len(matrix[0])):
                 if matrix[row][col] == '1':
@@ -10,6 +10,6 @@ class Solution:
                         dp[row][col] = 1
                     else:
                         dp[row][col] = min(dp[row-1][col], dp[row][col-1], dp[row-1][col-1]) + 1
-                    max_size = max(max_size, dp[row][col])
+                    max_side = max(max_side, dp[row][col])
 
-        return max_size * max_size
+        return max_side * max_side
